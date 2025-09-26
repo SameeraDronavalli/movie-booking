e95aee2c74ffac3bf064f221ca5cc6d4596b82da
SameeraDronavalli <sameeradronavalli327@gmail.com>
2025-09-26 19:12:26 +0530

    Update movie.js

--- movie.js
+++ movie.js
@@ -7,13 +7,13 @@ const cinemas = {
             title: "Sankranthiki Vastunam",
             seats: [13, 14],
             shows: ["9am", "6pm"],
-            Image:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OS8xMCAgNjQuN0sgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00418119-urxwxzudkv-portrait.jpg",
+            Image:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/sankranthiki-vasthunam-et00418119-1731656543.jpg"
         },
         {
             title: "Game Changer",
             seats: [11, 12],
             shows: ["11am", "9pm"],
-            Image:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-Ny40LzEwICA4OEsgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00311772-dyqxbymvgu-portrait.jpg",
+            Image:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/game-changer-et00407895-1723290122.jpg",
         },
         {
             title: "Identity",
